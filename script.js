0803a2650ae926295bf483f2402bb12414f836bf
straight-code <97550737+straight-code@users.noreply.github.com>
2023-03-27 15:36:32 +0100

    v1.0.0

---
+++ script.js
@@ -0,0 +1,16 @@
+WebFontConfig = {
+   google: { families: [ 'Roboto+Slab::latin', 'Titillium Web', 'Permanent Marker'  ] }
+  };
+  (function() {
+    var wf = document.createElement('script');
+    wf.src = 'https://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
+    tps://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
+    wf.type = 'text/javascript';
+    wf.async = 'true';
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(wf, s);
+  })();
+
+$(document).ready(function(){
+  // jQuery methods go here  
+})
